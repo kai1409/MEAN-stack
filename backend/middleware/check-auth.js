608b512b96001dcb44954effc4462b777e2c5f33
kai1409 <kasturi.nandi1998@gmail.com>
2021-07-05 11:54:53 +0530

    changes12

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req,res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1]; // Bearer afllfdfkjfdfcvsdljflm5
-        jwt.verify(token,"MyKey");
+        jwt.verify(token,APP_KEY);
         next();
     }
     catch(error) {
